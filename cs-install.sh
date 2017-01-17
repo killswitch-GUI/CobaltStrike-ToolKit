b09b20cbed0e77bf26afae1893fc5d158c0b21cd
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2017-01-17 09:59:10 -0500

    Create cs-install.sh

---
+++ cs-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# adapted from @christruncer
+# just and example
+wget https://www.cobaltstrike.com/downloads/a24a41fcae308883d74f3b57e36e5bbb/cobaltstrike-trial.tgz
+tar zxvf cobaltstrike-trial.tgz
+sudo apt-get update -y
+sudo apt-get install build-essential
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update -y 
+sudo apt-get install oracle-java7-installer
