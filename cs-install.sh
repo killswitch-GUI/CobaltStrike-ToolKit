74b3f9dcd5bee1c3703a2c4be1ff48c583a9fd4a
Alexander Rymdeko-Harvey <killswitch-GUI@users.noreply.github.com>
2017-01-17 15:39:15 -0500

    Update cs-install.sh

--- cs-install.sh
+++ cs-install.sh
@@ -4,7 +4,7 @@
 wget https://www.cobaltstrike.com/downloads/a24a41fcae308883d74f3b57e36e5bbb/cobaltstrike-trial.tgz
 tar zxvf cobaltstrike-trial.tgz
 sudo apt-get update -y
-sudo apt-get install build-essential
-sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get install build-essential -y 
+sudo add-apt-repository ppa:webupd8team/java -y 
 sudo apt-get update -y 
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java7-installer -y 
